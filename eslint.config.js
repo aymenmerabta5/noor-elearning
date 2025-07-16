dd9b42f96258dfac22d3d2270096db64f99fb181
Aimen Merabta <aymenmerabta5@gmail.com>
2025-07-16 14:31:51 +0100

    feat(auth): just install better auth and add some things...

--- eslint.config.js
+++ eslint.config.js
@@ -34,6 +34,7 @@ export default tseslint.config(
         { argsIgnorePattern: "^_" },
       ],
       "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
       "@typescript-eslint/no-misused-promises": [
         "error",
         { checksVoidReturn: { attributes: false } },
